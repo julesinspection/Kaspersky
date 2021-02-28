4ab3dab9448d0cf243db6e60f441ec7e194aacb4
julesinspection <julesinspection@users.noreply.github.com>
2021-02-28 19:14:08 +0300

    finally

---
+++ Kaspersky/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Kaspersky
+{
+
+    public class FileProcessor {
+        public string extension { get; private set; } //формат файла
+        public string fileFullName { get; private set; } //имя файла + формат
+        public string filePath { get; private set; } //путь к файлу
+
+        //Directory.GetCurrentDirectory() возвращает путь к папке ..\Kaspersky\Kaspersky\bin\Debug\netcoreapp3.1
+        //чтобы перейти к папке с проектом Visual Studio нам необходимо подняться на 4 каталога вверх
+        public string GetMyPath() => Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName;
+
+        public void ProcessFile(string fileName)
+        {
+            int i, numberOfFile; //перечислитель, номер файла который выбирает пользователь в консоли
+            //вычисляем путь к корневой папке проекта
+            string path = GetMyPath();
+
+            //поиск файлов с таким именем любого формата по заданному пути
+            string[] files = Directory.GetFiles(path, fileName + ".*", SearchOption.AllDirectories);
+
+            //вывод в консоль всех файлов и их путей с данным именем любого формата
+            for (i = 0; i < files.Length; i++) {
+                Console.WriteLine("{0}>  {1}", i + 1, files[i]);
+            }
+
+            try {
+                //ввод номера файла из консоли
+                Console.WriteLine("Выберите файл> ");
+                numberOfFile = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                //вычиление формата файла
+                extension = files[numberOfFile].Substring(files[numberOfFile].LastIndexOf('.'));
+
+                //вычисление полного имени и пути к файлу
+                fileFullName = fileName + extension;
+                filePath = files[numberOfFile].Replace(fileFullName, "");
+
+                //здесь мы можем проводить любые операции с файлом
+                using StreamWriter file = new StreamWriter(filePath + fileFullName, true, Encoding.Default);
+                file.WriteLine("hello");//например допишем строку hello
+            }
+            catch { //исключение на случай если пользователь ввел номер несуществующего решения
+                Console.WriteLine("Неверно введенные данные");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = Console.ReadLine();
+
+            FileProcessor f = new FileProcessor();
+            f.ProcessFile(fileName);
+            //здесь мы можем проводить любые операции с файлом
+            Console.ReadKey();
+        }
+    }
+}
---
+++ KasperskyTests/FileProcessorTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kaspersky;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kaspersky.Tests {
+    [TestClass()]
+    public class FileProcessorTests {
+        [TestMethod()]
+        //тест проверяет правильно ли программа вычисляет путь к корневому каталогу проекта
+        public void GetMyPathTest()
+        {
+            string MyPath = @"C:\Users\Григорий\source\repos\Kaspersky"; //вручную вводим путь
+            FileProcessor p = new FileProcessor();
+            Assert.AreEqual(MyPath, p.GetMyPath()); //сравниваем с результатом работы метода GetMyPath
+        }
+    }
+}
